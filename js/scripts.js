7793194eaa982373fa0835344a9f4c4d7ab461fe
gricelsepulveda <gricel@gricelsepulveda.com>
2016-05-12 13:47:00 -0300

    vista publicados y correcion en modal imagenes

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,44 @@ $(document).ready(function(){
         $(".item > a > li").removeClass("activo");
         $(this).addClass("activo");
     });
+    $(".imagenes_lista > li").click(function(){
+        $(".imagenes_lista > li").removeClass("activo");
+        $(this).addClass("activo");
+    });
+    /*IMAGENES DESTACADAS*/
+    $("#i_destacada").click(function(){
+        $(".imagen_destacada").fadeIn("slow");
+        $(".imagen_1").fadeOut("fast");
+        $(".imagen_2").fadeOut("fast");
+        $(".imagen_3").fadeOut("fast");
+        $(".imagen_4").fadeOut("fast");
+    });
+    $("#i_1").click(function(){
+        $(".imagen_1").fadeIn("slow");
+        $(".imagen_destacada").fadeOut("fast");
+        $(".imagen_2").fadeOut("fast");
+        $(".imagen_3").fadeOut("fast");
+        $(".imagen_4").fadeOut("fast");
+    });
+    $("#i_2").click(function(){
+        $(".imagen_2").fadeIn("slow");
+        $(".imagen_destacada").fadeOut("fast");
+        $(".imagen_1").fadeOut("fast");
+        $(".imagen_3").fadeOut("fast");
+        $(".imagen_4").fadeOut("fast");
+    });
+    $("#i_3").click(function(){
+        $(".imagen_3").fadeIn("slow");
+        $(".imagen_1").fadeOut("fast");
+        $(".imagen_2").fadeOut("fast");
+        $(".imagen_destacada").fadeOut("fast");
+        $(".imagen_4").fadeOut("fast");
+    });
+    $("#i_4").click(function(){
+        $(".imagen_4").fadeIn("slow");
+        $(".imagen_1").fadeOut("fast");
+        $(".imagen_2").fadeOut("fast");
+        $(".imagen_3").fadeOut("fast");
+        $(".imagen_destacada").fadeOut("fast");
+    });
 });
